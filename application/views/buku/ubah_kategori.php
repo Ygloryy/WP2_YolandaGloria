7bf4395fee70a4e9042df2d5c6a6ccdebcdb67fb
Ygloryy <gloryxzv@gmail.com>
2024-06-05 17:55:00 +0700

    Pertemuan 11 dan Tugas 9

---
+++ application/views/buku/ubah_kategori.php
@@ -0,0 +1,24 @@
+<div class="container-fluid">
+    <?= $this->session->flashdata('pesan'); ?>
+    <div class="row">
+        <div class="col-lg-6">
+            <?php if (validation_errors()) {
+                $this->session->set_flashdata('pesan', '<div class="alert alert-danger alert-message" role="alert">Nama Kategori tidak boleh kosong</div>');
+                redirect('buku/ubah_kategori/' . $k['id']);
+            } ?>
+            <?php foreach ($kategori as $k) { ?>
+                <form action="<?= base_url('buku/ubahkategori'); ?>" method="post">
+                    <div class="form-group">
+                        <input type="hidden" name="id" id="id" value="<?php echo $k['id']; ?>">
+                        <input type="text" class="form-control form-control-user" id="kategori" name="kategori" placeholder="Masukan Kategori Buku" value="<?= $k['kategori']; ?>">
+                    </div>
+                    <div class="form-group">
+                        <input type="button" class="form-control form form-control-user btn btn-dark col-lg-3 mt-3 " value="Kembali" onclick="window.history.go(-1)">
+                        <input type="submit" class="form-control form-control-user btn btn-primary col-lg-3 mt-3" value="update">
+                    </div>
+                </form>
+            <?php } ?>
+        </div>
+    </div>
+</div>
+</div>

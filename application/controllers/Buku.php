d04efb055c4530832fdb7f461e9be3cb96a6d113
Ygloryy <gloryxzv@gmail.com>
2024-06-10 10:02:27 +0700

    Pertemuan 12 dan Tugas 10

--- application/controllers/Buku.php
+++ application/controllers/Buku.php
@@ -48,7 +48,6 @@ class Buku extends CI_Controller
             'numeric' => 'Yang anda masukan bukan angka'
         ]);
 
-        //konfigurasi sebelum gambar diupload
         $config['upload_path'] = './assets/img/upload/';
         $config['allowed_types'] = 'jpg|png|jpeg';
         $config['max_size'] = '3000';
